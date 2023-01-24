d9fda7c041c155857d466fa2b3b6fe1b965784dd
Rahim <abdourahim.diouf@supdeco.edu.sn>
2023-01-24 15:03:14 +0000

    Ajout du fichier rahim et creation pages

---
+++ resources/views/rahim/css/animation.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.banner').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+});
